bc078a9b207c2525aeab701e7b933a1441c03598
motsf <master.of.the.real@gmail.com>
2024-09-20 01:27:55 +0900

    initial commit

---
+++ app/src/main/java/com/thereal/fun/bingo_0/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.thereal.`fun`.bingo_0
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.thereal.`fun`.bingo_0.ui.theme.Bingo_0Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Bingo_0Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!\nThis app DO NOTHING \nbut just placeholder for preventing account closing\nSo please don't pay for this app",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Bingo_0Theme {
+        Greeting("Android")
+    }
+}
